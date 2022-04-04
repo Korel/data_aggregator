4b8067a3682a40bbe00da38553fa1b9cc16382e9
Korel <korelerben@gmail.com>
2022-04-04 14:38:46 +0200

    Refactoring (needs testing), Redis connection needs to be implemented

--- main.go
+++ main.go
@@ -7,39 +7,40 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
-	config, err := Parse("config.yaml")
+func failOnError(err error, str string) {
 	if err != nil {
-		log.Fatalf("error parsing config: %s", err)
+		log.Fatalf("%s: %s", str, err)
 	}
+}
 
-	log.Printf("RabbitMQ Source Address: %s\n", config.AmqSource.Broker.Address)
-	log.Printf("RabbitMQ Target Address: %s\n", config.AmqTarget.Broker.Address)
+func setupRedis(config *Config) {
+	//TODO: find a library and implement
+}
 
-	receiverConn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/", config.AmqSource.Credentials.Username, config.AmqSource.Credentials.Password, config.AmqSource.Broker.Address, config.AmqSource.Broker.Port))
-	if err != nil {
-		log.Fatalf("Error connecting to source RabbitMQ: %s", err)
-	}
+func setupRabbitmq(config *Config, consumeCallback func(*PubSubConfig, <-chan amqp.Delivery, *amqp.Channel)) {
+	sourceConn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/",
+		config.AmqSource.Credentials.Username,
+		config.AmqSource.Credentials.Password,
+		config.AmqSource.Broker.Address,
+		config.AmqSource.Broker.Port))
+	failOnError(err, "Failed to connect to source RabbitMQ")
 	log.Printf("Connected to source RabbitMQ")
-
-	forwardConn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/", config.AmqTarget.Credentials.Username, config.AmqTarget.Credentials.Password, config.AmqTarget.Broker.Address, config.AmqTarget.Broker.Port))
-	if err != nil {
-		log.Fatalf("Error connecting to target RabbitMQ: %s", err)
-	}
+	targetConn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/",
+		config.AmqTarget.Credentials.Username,
+		config.AmqTarget.Credentials.Password,
+		config.AmqTarget.Broker.Address,
+		config.AmqTarget.Broker.Port))
+	failOnError(err, "Failed to connect to target RabbitMQ")
 	log.Printf("Connected to target RabbitMQ")
-
+	// Create pub_sub channels, declare exchanges and queues
 	for _, pubSub := range config.PubSub {
 		for _, pb := range pubSub {
-			rchan, err := receiverConn.Channel()
-			if err != nil {
-				log.Fatalf("Error getting source channel: %s", err)
-			}
-			fchan, err := forwardConn.Channel()
-			if err != nil {
-				log.Fatalf("Error getting target channel: %s", err)
-			}
+			sourceChannel, err := sourceConn.Channel()
+			failOnError(err, "Failed to open the source channel")
+			targetChannel, err := targetConn.Channel()
+			failOnError(err, "Failed to open the target channel")
 
-			if err := rchan.ExchangeDeclare(
+			err = sourceChannel.ExchangeDeclare(
 				pb.Source.Exchange,     // exchange name
 				pb.Source.ExchangeType, // exchange type
 				false,                  // durable
@@ -47,12 +48,11 @@ func main() {
 				false,                  // internal
 				false,                  // noWait
 				nil,                    // arguments
-			); err != nil {
-				log.Fatalf("Error declaring source exchange: %s", err)
-			}
+			)
+			failOnError(err, "Failed to declare the source exchange")
 			log.Printf("Declared source exchange: %s", pb.Source.Exchange)
 
-			if err = fchan.ExchangeDeclare(
+			err = targetChannel.ExchangeDeclare(
 				pb.Target.Exchange,     // exchange name
 				pb.Target.ExchangeType, // exchange type
 				false,                  // durable
@@ -60,12 +60,11 @@ func main() {
 				false,                  // internal
 				false,                  // noWait
 				nil,                    // arguments
-			); err != nil {
-				log.Fatalf("Error declaring target exchange: %s", err)
-			}
+			)
+			failOnError(err, "Failed to declare the target exchange")
 			log.Printf("Declared target exchange: %s", pb.Target.Exchange)
 
-			rqueue, err := rchan.QueueDeclare(
+			sourceQueue, err := sourceChannel.QueueDeclare(
 				pb.Source.Queue, // queue name
 				true,            // durable
 				false,           // delete when unused
@@ -73,12 +72,10 @@ func main() {
 				false,           // no-wait
 				nil,             // arguments
 			)
-			if err != nil {
-				log.Fatalf("Error declaring source queue: %s", err)
-			}
+			failOnError(err, "Failed to declare the source queue")
 			log.Printf("Declared source queue: %s", pb.Source.Queue)
 
-			fqueue, err := fchan.QueueDeclare(
+			targetQueue, err := targetChannel.QueueDeclare(
 				pb.Target.Queue, // queue name
 				true,            // durable
 				false,           // delete when unused
@@ -86,72 +83,77 @@ func main() {
 				false,           // no-wait
 				nil,             // arguments
 			)
-			if err != nil {
-				log.Fatalf("Error declaring target queue: %s", err)
-			}
+			failOnError(err, "Failed to declare the target queue")
 			log.Printf("Declared target queue: %s", pb.Target.Queue)
 
-			if err = rchan.QueueBind(
-				rqueue.Name,        // queue name
+			err = sourceChannel.QueueBind(
+				sourceQueue.Name,   // queue name
 				"",                 // routing key
 				pb.Source.Exchange, // exchange
 				false,              // noWait
 				nil,                // arguments
-			); err != nil {
-				log.Fatalf("Error binding source queue: %s", err)
-			}
+			)
+			failOnError(err, "Failed to bind the source queue")
 			log.Printf("Bound to source queue: %s", pb.Source.Queue)
 
-			if err = fchan.QueueBind(
-				fqueue.Name,        // queue name
+			err = targetChannel.QueueBind(
+				targetQueue.Name,   // queue name
 				"",                 // routing key
 				pb.Target.Exchange, // exchange
 				false,              // noWait
 				nil,                // arguments
-			); err != nil {
-				log.Fatalf("Error binding target queue: %s", err)
-			}
+			)
+			failOnError(err, "Failed to bind the target queue")
 			log.Printf("Bound to target queue: %s", pb.Target.Queue)
 
-			deliveries, err := rchan.Consume(
-				rqueue.Name, // queue name
-				"",          // consumer
-				false,       // auto-ack
-				false,       // exclusive
-				false,       // no-local
-				false,       // no-wait
-				nil,         // arguments
+			deliveries, err := sourceChannel.Consume(
+				sourceQueue.Name, // queue name
+				"",               // consumer
+				false,            // auto-ack
+				false,            // exclusive
+				false,            // no-local
+				false,            // no-wait
+				nil,              // arguments
 			)
-			if err != nil {
-				log.Fatalf("Error consuming source queue: %s", err)
-			}
-
+			failOnError(err, "Failed to start consuming the source queue")
 			log.Printf("Starting to consume source queue: %s", pb.Source.Queue)
 
-			go func(pb *PubSubConfig) {
-				for d := range deliveries {
-					if err := d.Ack(false); err != nil {
-						log.Fatalf("Error acknowledging delivery: %s", err)
-					}
-
-					// TODO: Filtering will be implemented here
-					// Filter(d.Body)
-
-					fchan.Publish(
-						pb.Target.Exchange, // exchange
-						"",                 // routing key
-						false,              // mandatory
-						false,              // immediate
-						amqp.Publishing{ // message
-							ContentType: "text/plain",
-							Body:        d.Body,
-						},
-					)
-				}
-			}(&pb)
-
+			go consumeCallback(&pb, deliveries, targetChannel)
 		}
 	}
+}
+
+func rabbitmqConsumeCallback(pb *PubSubConfig, deliveries <-chan amqp.Delivery, targetChannel *amqp.Channel) {
+	for d := range deliveries {
+		if err := d.Ack(false); err != nil {
+			log.Fatalf("Error acknowledging delivery: %s", err)
+		}
+
+		// TODO: Filtering will be implemented here
+		// Filter(d.Body)
+
+		targetChannel.Publish(
+			pb.Target.Exchange, // exchange
+			"",                 // routing key
+			false,              // mandatory
+			false,              // immediate
+			amqp.Publishing{ // message
+				ContentType: "text/plain",
+				Body:        d.Body,
+			},
+		)
+	}
+}
+
+func main() {
+	config, err := Parse("config.yaml")
+	failOnError(err, "Failed to parse config file")
+	log.Printf("RabbitMQ Source Address: %s\n", config.AmqSource.Broker.Address)
+	log.Printf("RabbitMQ Target Address: %s\n", config.AmqTarget.Broker.Address)
+
+	setupRedis(config)
+
+	setupRabbitmq(config, rabbitmqConsumeCallback)
 
 	// The consuming and forwarding are done in goroutines.
 	select {} // block foreve
